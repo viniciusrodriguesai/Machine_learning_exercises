6deec5f5aa2ca4c192f2f9c1c4310d651d477eb8
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-25 20:34:58 -0300

    Import Pocket PLA into the linear regression classifier

--- Third_Practical_Lesson/lrclassifier.py
+++ Third_Practical_Lesson/lrclassifier.py
@@ -1,22 +1,26 @@
 from linearregression import LinearRegression
+from pocketpla import PocketPLA
 import numpy as np
-#from pocketpla import PocketPLA
 
 
-class LRClassifier():
+class LRClassifier:
     def execute(self, _X, _y):
         lr = LinearRegression()
         lr.execute(_X, _y)
+
         self.w = lr.get_w()
-        
-        #pla = PocketPLA()
-        #pla.set_w(self.w)
-        #pla.execute(_X, _y)
-        #self.w = pla.get_w()
-        
-        
+
+        # O Pocket PLA será aplicado aqui
+
     def predict(self, x_):
-        return np.sign([np.dot(self.w, x) for x in x_])
-     
+        return np.sign([
+            np.dot(self.w, x)
+            for x in x_
+        ])
+
     def getRegressionY(self, regressionX, shift=0):
-        return (-self.w[0]+shift - self.w[1]*regressionX) / self.w[2]
+        return (
+            -self.w[0]
+            + shift
+            - self.w[1] * regressionX
+        ) / self.w[2]
